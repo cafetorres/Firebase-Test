ce30a7b3259e3575790fee9f0381094e0dd637ee
Carlos Torres <cf.torresluna@gmail.com>
2016-11-17 09:58:36 -0600

    add read from firebase

--- app/src/main/java/me/cafetorres/dicistest/MainActivity.java
+++ app/src/main/java/me/cafetorres/dicistest/MainActivity.java
@@ -2,11 +2,16 @@ package me.cafetorres.dicistest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,11 +34,26 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         database= FirebaseDatabase.getInstance();
         myReference=database.getReference("message");
+
+        myReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                Log.d("Value is:",value);
+                Toast.makeText(MainActivity.this,value,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("Failed to read value.", databaseError.toException());
+            }
+        });
     }
 
     @OnClick( R.id.button)
     public void clickSubmitHandler(){
        String message=editText.getText().toString();
         myReference.setValue(message);
+        editText.setText("");
     }
 }
