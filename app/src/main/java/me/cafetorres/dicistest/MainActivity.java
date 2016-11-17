d7f9ca29f4efe3e9324b2e4cf105bf618897400e
Carlos Torres <cf.torresluna@gmail.com>
2016-11-17 09:30:45 -0600

    add butterknife in mainactivity

--- app/src/main/java/me/cafetorres/dicistest/MainActivity.java
+++ app/src/main/java/me/cafetorres/dicistest/MainActivity.java
@@ -1,13 +1,30 @@
 package me.cafetorres.dicistest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.editText)
+    EditText editText;
+    @Bind(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    
+    @OnClick R.id.button
+    public void clickSubmitHandler(){
+
     }
 }
